a59752a8f0e4d1ba506c13a849bb700993bba141
Timothy Gaweco <tim.gaweco@gmail.com>
2013-11-18 21:57:36 -0500

    first commit

---
+++ k-means.py
@@ -0,0 +1,48 @@
+import random
+
+#
+# K-means clustering algorithm implementation
+# By: Timothy Gaweco
+#
+
+# Example data
+points = [(1,2),(3,5),(5,7),(10,5),(15,7),(20,10),(65,12),(56,15),(4,5)]
+
+# Distance function
+def distance():
+    dist = 0
+
+
+
+    return dist
+
+# K-means function
+def kmeans(points, k):
+    clusters = []
+
+    # Find max and min in each dimension
+    for p in points:
+        
+
+
+        print
+
+
+
+    # Randomly assign k cluster centroids
+    for i in range(k):
+
+
+
+        print
+
+    # Assign each point to closest centroid
+
+    # Compute new average of points assigned to cluster k
+
+
+    return clusters
+
+
+if __name__ == '__main__':
+    print
