4e4204321f59a9d7134a45d92597ca3836972db9
Timothy Gaweco <tim.gaweco@gmail.com>
2013-11-20 22:56:27 -0500

    more changes to k-means function

--- k-means.py
+++ k-means.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 
 #
 # K-means clustering algorithm implementation
@@ -42,23 +43,37 @@ def kmeans(points, k):
         for j in range(len(maximum)):
             number = random.random() * (maximum[j] - minimum[j]) + minimum[j]
             centroid += (number,)
-        centroids.append(centroid)
+        centroids.append((centroid, i)) # add centroid number
         centroid = ()
 
     print centroids
 
-    clusters = [(1000000,'Cluster 1') for i in range(len(points))]
-    pos = 0
+    clusters = [ [] for i in range(k)]
+    min_dist = 0
+    centroid_num = 0
     # Assign each point to closest centroid
     for p in points:
+        # Find closest centroid
         for c in centroids:
             dist = distance(p, c)
 
-            if dist < clusters[pos][0]:
-                clusters[pos] = (dist, ) #add cluster number?
+            if dist < min_dist:
+                min_dist = dist
+                centroid_num = c[1]
+
+        clusters[centroid_num-1].append(p)
+
+    # Compute new average of points assigned to cluster k, change centroids
+    points_in_cluster = []
+    for cluster in clusters:
+        for p in cluster:
+            points_in_cluster.append(p[0])
+
+        
+
+        np.average(points_in_cluster)
 
 
-    # Compute new average of points assigned to cluster k
 
 
     return clusters
